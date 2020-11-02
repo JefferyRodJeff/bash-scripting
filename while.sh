f9bc407e6fab646f3edaab8c6f6a7188c4d3c3bc
AaronQ20 <71231972+AaronQ20@users.noreply.github.com>
2020-11-01 21:16:36 -0700

    Update while.sh

--- while.sh
+++ while.sh
@@ -5,3 +5,8 @@ while [ $counter -lt 3 ]; do
 	let counter+=1
 	echo $counter
 done
+
+#the while tests against the counter which is equal to 0. 
+#the block adds 1 to counter while it is less than 3. 
+#then it displays (echos) the counter. 
+#this script was good and understandable 
