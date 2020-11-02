f21edf667e023d3b32f03f4ec0b4bcf05df324d3
AaronQ20 <71231972+AaronQ20@users.noreply.github.com>
2020-11-01 21:13:33 -0700

    Update until.sh

--- until.sh
+++ until.sh
@@ -5,3 +5,8 @@ until [ $counter -lt 3 ]; do
 	let counter-=1
 	echo $counter
 done
+
+#the script sets counter to 6.
+#the block tests against counter stating until the counter is less than 6, subtract 1 from counter.
+#the block then echos the counter. 
+#This was good 
